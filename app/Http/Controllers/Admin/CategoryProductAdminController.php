79c06da3f04fce653453ccb5869ccb676480fa69
altamisatmaja <altamisfattah16@gmail.com>
2024-04-29 10:45:42 +0700

    fix: crud

--- app/Http/Controllers/Admin/CategoryProductAdminController.php
+++ app/Http/Controllers/Admin/CategoryProductAdminController.php
@@ -75,8 +75,12 @@ class CategoryProductAdminController extends Controller
     public function update(Request $request, $slug_kategori_product)
     {
         try {
-            $categoryproduct = CategoryProduct::where('slug_kategori_product', $slug_kategori_product);
-
+            $categoryproduct = CategoryProduct::where('slug_kategori_product', $slug_kategori_product)->first();
+    
+            if (!$categoryproduct) {
+                return redirect()->back()->with('error', 'Data kategori produk tidak ditemukan');
+            }
+    
             $validator = Validator::make($request->all(), [
                 'nama_kategori_product' => 'required',
                 'deskripsi_kategori_product' => 'required',
@@ -88,15 +92,16 @@ class CategoryProductAdminController extends Controller
                 'gambar_kategori_product.mimes' => 'Gambar kategori produk harus memiliki format file jpg, png, jpeg, atau webp.',
                 'gambar_kategori_product.max' => 'Gambar kategori produk harus berukuran 1MB kebawah',
             ]);
-
+    
             if ($validator->fails()) {
                 return redirect()->back()->withErrors($validator)->withInput();
             }
-
+    
             $input = $request->except(['_token', '_method' ]);
-
+    
             if ($request->hasFile('gambar_kategori_product')) {
                 File::delete('uploads/' . $categoryproduct->gambar_kategori_product);
+    
                 $gambar = $request->file('gambar_kategori_product');
                 $nama_gambar = time() . rand(1, 9) . '.' . $gambar->getClientOriginalExtension();
                 $gambar->move('uploads', $nama_gambar);
@@ -104,39 +109,48 @@ class CategoryProductAdminController extends Controller
             } else {
                 unset($input['gambar_kategori_product']);
             }
-
+    
             $slug = $this->generateSlug($input['nama_kategori_product']);
             $input['slug_kategori_product'] = $slug;
-
+    
             $categoryproduct->update($input);
-
-            return redirect()->route('admin.category.list')->with('success', 'Data kategori product berhasil diubah');
+    
+            return redirect()->route('admin.category.list')->with('success', 'Data kategori produk berhasil diubah');
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
         }
     }
+    
 
     public function destroy($slug_kategori_product)
     {
-        $categoryProduct = CategoryProduct::where('slug_kategori_product', $slug_kategori_product)->first();
+        try {
+            $categoryproduct = CategoryProduct::where('slug_kategori_product', $slug_kategori_product)->first();
 
-        if ($categoryProduct) {
-            File::delete('uploads/' . $categoryProduct->gambar_kategori_product);
+        if ($categoryproduct) {
+            File::delete('uploads/' . $categoryproduct->gambar_kategori_product);
 
-            $categoryProduct->delete();
+            $categoryproduct->delete();
 
             return redirect()->route('admin.category.list')->with('success', 'Data berhasil dihapus');
         } else {
             return redirect()->route('admin.category.list')->with('error', 'Data tidak ditemukan');
+        }
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
         }
     }
 
 
     public function generateSlug($nama_product)
     {
-        $slug = strtolower($nama_product);
-        $slug = str_replace(' ', '-', $slug);
+        try {
+            $slug = strtolower($nama_product);
+            $slug = str_replace(' ', '-', $slug);
 
-        return $slug;
+            return $slug;
+        } catch (\Exception $e) {
+            throw 'Gagal mendapatkan slug';
+        }
     }
 }
