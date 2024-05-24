3d00c66f4ec831ab619c1008eab0b7a993628e19
altamisatmaja <altamisfattah16@gmail.com>
2024-05-25 06:15:02 +0700

    fix: report by partner

--- resources/views/partner/pages/report/index.blade.php
+++ resources/views/partner/pages/report/index.blade.php
@@ -4,13 +4,117 @@
 
 @section('content')
 
-    <section class="container px-4 mx-auto">
-        <div class="flex flex-col">
+    <section class="container mx-auto">
+        <div class="pb-5">
+            <ol class="flex items-start gap-4">
+                <li>
+                    <div
+                        class="flex items-center text-lg font-medium transition-all duration-300 hover:text-primarybase">
+                        <svg class="mr-2.5 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                        </svg>
+                        <a href="{{ route('partner.dashboard') }}">Beranda </a>
+                    </div>
+                </li>
+                <li class="inline-flex">
+                    <div
+                        class="flex items-center gap-2 text-lg font-medium transition-all duration-300 hover:text-primarybase">
+                        <svg class="h-3 w-3 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <a href="{{ route('partner.report.list') }}"> Laporan </a>
+                    </div>
+                </li>
+            </ol>
+        </div>
+        @if (session('success'))
+            <div id="successMessage"
+                class="fixed top-0 left-0 w-full h-full flex justify-center items-center backdrop-blur-md bg-white/30 bg-opacity-50 z-50">
+                <div class="relative w-full max-w-screen-md rounded-lg bg-green-500 px-4 py-4 text-base text-white"
+                    data-dismissible="alert">
+                    <div class="absolute top-4 left-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+                            class="mt-px h-6 w-6">
+                            <path fill-rule="evenodd"
+                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <div class="ml-8 mr-12">
+                        <h5
+                            class="block font-sans text-xl font-semibold leading-snug tracking-normal text-white antialiased">
+                            Berhasil
+                        </h5>
+                        <p class="mt-2 block font-sans text-base font-normal leading-relaxed text-white antialiased">
+                            {{ session('success') }}
+                        </p>
+                    </div>
+                    <div data-dismissible-target="alert" data-ripple-dark="true"
+                        class="absolute top-3 right-3 w-max rounded-lg transition-all hover:bg-white hover:bg-opacity-20">
+                        <div role="button" class="w-max rounded-lg p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
 
+        @if (session('error'))
+            <div id="successMessage"
+                class="fixed top-0 left-0 w-full h-full flex justify-center items-center backdrop-blur-md bg-white/30 bg-opacity-50 z-50">
+                <div class="relative w-full max-w-screen-md rounded-lg bg-red-500 px-4 py-4 text-base text-white"
+                    data-dismissible="alert">
+                    <div class="absolute top-4 left-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"
+                            class="mt-px h-6 w-6">
+                            <path fill-rule="evenodd"
+                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <div class="ml-8 mr-12">
+                        <h5
+                            class="block font-sans text-xl font-semibold leading-snug tracking-normal text-white antialiased">
+                            Gagal
+                        </h5>
+                        <p class="mt-2 block font-sans text-base font-normal leading-relaxed text-white antialiased">
+                            {{ session('error') }}
+                        </p>
+                    </div>
+                    <div data-dismissible-target="alert" data-ripple-dark="true"
+                        class="absolute top-3 right-3 w-max rounded-lg transition-all hover:bg-white hover:bg-opacity-20">
+                        <div role="button" class="w-max rounded-lg p-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="flex flex-col">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div class="mb-5 w-full">
+                        <div class="mb-3">
+                            <h3 class="font-semibold text-2xl text-textbase">Laporan 👋</h3>
+                        </div>
+                        <p class="text-lg text-textbase">
+                            Lihat transaksi yang sudah selesai ya!
+                        </p>
+                    </div>
                     <div class="overflow-hidden border border-gray-200  md:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
+                        <table class="divide-y divide-gray-200 w-full">
                             <thead class="bg-gray-50 ">
                                 <tr>
                                     <th scope="col"
@@ -39,12 +143,21 @@
 
                                     <th scope="col"
                                         class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                                        Harga produk
+                                    </th>
+
+                                    <th scope="col"
+                                        class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
                                         Kuantitas
                                     </th>
 
                                     <th scope="col"
                                         class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
                                         Total
+                                    </th>
+                                    <th scope="col"
+                                        class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
+                                        Catatan
                                     </th>
                                 </tr>
                             </thead>
@@ -59,34 +172,34 @@
                                         <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{{ date('D, m, Y', strtotime($report['dipesan_pada'])) }}</td>
                                         <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
                                             <div class="flex items-center gap-x-2">
-                                                <img class="object-cover w-8 h-8 rounded-full"
+                                                <img class="object-cover w-8 h-8 rounded-lg"
                                                     src="/uploads/{{ $report['gambar_pembeli'] }}"
                                                     alt="">
-                                                <div>
-                                                    <h2 class="text-sm font-medium text-gray-800  ">{{ $report['nama_pembeli'] }}</h2>
-                                                    <p class="text-xs font-normal text-gray-600">
+                                                    <div>
+                                                        <h2 class="text-sm font-medium text-gray-800  ">{{ $report['nama_pembeli'] }}</h2>
+                                                        <p class="text-xs font-normal text-gray-600">
                                                         {{ $report['lokasi_pengiriman'] }}</p>
-                                                </div>
+                                                    </div>
                                             </div>
                                         </td>
                                         <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">
                                             <div class="flex items-center gap-x-2">
-                                                <img class="object-cover w-8 h-8 rounded-full"
-                                                    src="/uploads/{{ $report['gambar_produk'] }}"
-                                                    alt="">
+                                                <img class="object-cover w-8 h-8 rounded-lg"
+                                                src="/uploads/{{ $report['gambar_produk'] }}"
+                                                alt="">
                                                 <div>
                                                     <h2 class="text-sm font-medium text-gray-800  ">{{ $report['nama_pembeli'] }}</h2>
                                                     <p class="text-xs font-normal text-gray-600">
                                                         {{ $report['nama_produk'] }}</p>
-                                                    <p class="text-xs font-normal text-gray-600">
+                                                        <p class="text-xs font-normal text-gray-600">
                                                         {{ $report['nama_kategori_produk'] }}</p>
-                                                    <p class="text-xs font-normal text-gray-600">
-                                                        {{ $report['harga_produk'] }}</p>
                                                 </div>
                                             </div>
                                         </td>
+                                        <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">@currency($report['harga_produk'])</td>
                                         <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $report['kuantitas'] }}</td>
-                                        <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $report['total_untung'] }}</td>
+                                        <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">@currency($report['total_untung'])</td>
+                                        <td class="px-4 py-4 text-sm text-gray-500 whitespace-nowrap">{{ $report['catatan'] == NULL ? 'Tidak ada' : $report['catatan'] }}</td>
                                         {{-- <td class="px-4 py-4 text-sm whitespace-nowrap">
                                             <a href="{{ route('partner.report.detail', $report['reference']) }}">
                                                 <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
@@ -110,41 +223,6 @@
             </div>
         </div>
 
-        <div class="flex items-center justify-between mt-6">
-            <a href="#"
-                class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100  ">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                </svg>
-
-                <span>
-                    previous
-                </span>
-            </a>
-
-            <div class="items-center hidden md:flex gap-x-3">
-                <a href="#" class="px-2 py-1 text-sm text-blue-500 rounded-md  bg-blue-100/60">1</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">2</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">3</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">...</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">12</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">13</a>
-                <a href="#" class="px-2 py-1 text-sm text-gray-500 rounded-md hover:bg-gray-100">14</a>
-            </div>
-
-            <a href="#"
-                class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100  ">
-                <span>
-                    Next
-                </span>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-5 h-5 rtl:-scale-x-100">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                </svg>
-            </a>
-        </div>
     </section>
 
 @endsection
